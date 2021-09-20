848090037d4883b4200a9a7799d2c707a68072a3
pradeep-selva <pradeepselvaraju@gmail.com>
2021-09-20 20:03:12 +0530

    initial commit

---
+++ contracts/Sample.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract Sample {
+    function test() public pure returns (string memory) {
+        return "TEST";
+    }
+}
---
+++ test/sample-test.js
@@ -0,0 +1,8 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("test", function () {
+  it("sample test", async function () {
+    expect(true).to.equal(true);
+  });
+});
